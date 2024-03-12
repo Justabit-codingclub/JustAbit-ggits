3b5f91cb4c56c94055ed4c36c04bdd606d0b86b4
Justabit-codingclub <163134510+Justabit-codingclub@users.noreply.github.com>
2024-03-12 18:58:17 +0530

    Create InputOutput.c

---
+++ myfirstprogram/InputOutpu.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+printf("Enter your name: "); //printf use to display output
+char str[100];
+  scanf("%s",&str ); //scanf used to take input
+  printf("Welcome %s, We are learning C.",str);
+}
